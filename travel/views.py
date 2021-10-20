db8f9fd4ac7acfe221842f065d5ade405b0ad571
ODMyk <ostapenko.dmytro05@gmail.com>
2021-10-20 22:30:13 +0300

    templates extending (base) added

--- travel/views.py
+++ travel/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home(request) -> HttpResponse:
+def home(request):
     name = "Dmytro"
 
     return render(request, "home.html", {"name": name})
